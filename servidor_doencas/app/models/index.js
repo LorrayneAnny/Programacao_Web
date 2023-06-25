31f06de8bf7013e414d379fcc64bf24d34db78f9
LorrayneAnny <lorrayneannyelly16@gmail.com>
2023-06-25 12:43:40 -0300

    V 05 adicionando rotas de usuários

--- servidor_doencas/app/models/index.js
+++ servidor_doencas/app/models/index.js
@@ -1,9 +1,11 @@
 const sequelize = require('../../database');
 const Doencas = require('./Doencas');
 const Medicamento = require('./Medicamento');
+const Usuarios = require('./Usuarios');
+
 
 const models = {
-  Doencas, Medicamento
+  Doencas, Medicamento, Usuarios
 };
 // Relacionamentos
 Medicamento.belongsTo(Doencas, {foreignKey: 'id_doenca', as: 'doenca'});
